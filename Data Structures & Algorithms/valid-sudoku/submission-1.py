a11a340e94d410a87b19b22c4786b31a86fa291d
Darren Kelly <92471463+DarrenGKelly@users.noreply.github.com>
2026-05-11 13:11:54 -0700

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.py
@@ -0,0 +1,23 @@
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        rows =  [set(), set(), set(), set(), set(), set(), set(), set(), set()]
+        cols =  [set(), set(), set(), set(), set(), set(), set(), set(), set()]
+        boxes = [set(), set(), set(), set(), set(), set(), set(), set(), set()]
+
+        # Create Hash Sets while checking
+        for i, row in enumerate(board):
+            for j, val in enumerate(row):
+                if (val == "."):
+                    continue
+
+                k = i // 3 * 3 + j // 3 
+                if (val in rows[i] or val in cols[j] or val in boxes[k]):
+                    return False
+                
+                rows[i].add(val)
+                cols[j].add(val)
+                boxes[k].add(val)
+
+        return True
